50cff2f853e70a10ce1b9beafe08d3f01824169d
Saul Vieira <vieira_saul@yahoo.com.br>
2021-06-23 22:24:17 -0300

    inserindo autocomplete de produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 import styled from "styled-components/macro";
-import TextBox from "devextreme-react/text-box";
+import { Autocomplete } from "devextreme-react/autocomplete";
 import { Button } from "devextreme-react/button";
 import { LoadPanel } from "devextreme-react/load-panel";
-import { getProducts } from "../../services/searchService";
+import { getProducts, getSuggestions } from "../../services/searchService";
+import CustomStore from "devextreme/data/custom_store";
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 900px;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  margin-top: 20px;
+  .dx-button {
+    margin-left: 20px;
+  }
+`;
+
+const StyledList = styled.ul`
+  margin-top: 10px;
+`;
 
 const Home = (props) => {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState(null);
   const [searchResults, setSearchResults] = useState([]);
   const [loadPanelVisible, setLoadPanelVisible] = useState(false);
 
@@ -14,12 +32,27 @@ const Home = (props) => {
     setSearchValue(e.value);
   };
 
+  const autoCompleteSource = new CustomStore({
+    load: () => {
+      if (searchValue !== "") {
+        return getSuggestions(searchValue);
+      }
+    },
+  });
+
+  const autoCompleteItemClick = async (e) => {
+    setLoadPanelVisible(true);
+    const data = await getProducts(e.itemData.term);
+    setLoadPanelVisible(false);
+    setSearchResults(data);
+  };
+
   const searchProduct = async (e) => {
     if (searchValue !== "") {
       setLoadPanelVisible(true);
       const data = await getProducts(searchValue);
       setLoadPanelVisible(false);
-      setSearchResults(data.products);
+      setSearchResults(data);
     }
   };
 
@@ -28,24 +61,6 @@ const Home = (props) => {
       <p>{name}</p>
     </li>
   ));
-
-  const Container = styled.div`
-    margin: 0 auto;
-    max-width: 900px;
-  `;
-
-  const SearchContainer = styled.div`
-    display: flex;
-    margin-top: 20px;
-    .dx-button {
-      margin-left: 20px;
-    }
-  `;
-
-  const StyledList = styled.ul`
-    margin-top: 10px;
-    list-style-type: none;
-  `;
 
   return (
     <React.Fragment>
@@ -56,10 +71,16 @@ const Home = (props) => {
       />
       <Container>
         <SearchContainer>
-          <TextBox
-            value={searchValue}
+          <Autocomplete
+            dataSource={autoCompleteSource}
+            valueExpr="term"
+            showClearButton={true}
+            defaultValue={searchValue}
             onValueChanged={searchValueChanged}
+            minSearchLength={3}
+            onItemClick={autoCompleteItemClick}
             width={250}
+            placeholder="Digite um produto..."
           />
           <Button
             text="Pesquisar"
@@ -68,7 +89,13 @@ const Home = (props) => {
             onClick={searchProduct}
           />
         </SearchContainer>
-        <StyledList>{productsList}</StyledList>
+        <StyledList>
+          {searchResults.length > 0 ? (
+            productsList
+          ) : (
+            <p>Não foram encontrados produtos com a palavra "{searchValue}".</p>
+          )}
+        </StyledList>
       </Container>
     </React.Fragment>
   );
--- src/services/searchService.js
+++ src/services/searchService.js
@@ -8,10 +8,27 @@ export async function getProducts(product) {
   );
 
   if (response.status >= 200 && response.status <= 299) {
-    return response.data;
+    return response.data.products;
   } else {
     return new Error(
       "Não foi possível carregar os produtos. Código do erro: " +
+        response.status
+    );
+  }
+}
+
+export async function getSuggestions(product) {
+  const response = await axios.get(
+    "https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=" +
+      product +
+      "&source=nanook"
+  );
+
+  if (response.status >= 200 && response.status <= 299) {
+    return response.data.suggestions;
+  } else {
+    return new Error(
+      "Não foi possível carregar as sugestões. Código do erro: " +
         response.status
     );
   }
